a0deec01d4bb9cd292ad62d85bc1f77f88a4f2ea
Alireza <Alirezakamyab19@gmail.com>
2020-12-13 17:53:51 +0330

    Solution for Project 27

--- QuadraticPrimes.cpp
+++ QuadraticPrimes.cpp
@@ -1,26 +1,34 @@
 //Created on: Dec 13, 2020 by Alireza
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int Quadratic(int, int, int);
 bool isPrime(int);
 
-int main(int argc, char ** argv){
+int main(int argc, char **argv)
+{
 	int maxPrimes = 0;
 	int maxi = 0, maxj = 0;
 	int numberPrimes = 0;
 	int counter = 0;
 
-	for(int i = -1000; i < 1000; i++){
-		for(int j = -1000; j <= 1000; j++){
+	for (int i = -1000; i < 1000; i++)
+	{
+		for (int j = -1000; j <= 1000; j++)
+		{
 			numberPrimes = 0;
 			counter = 0;
-			while (true){
+			while (true)
+			{
 				int result = Quadratic(i, j, counter);
-				if(isPrime(result)){
+				if (isPrime(result))
+				{
 					numberPrimes++;
-				}else{
-					if(maxPrimes < numberPrimes){
+				}
+				else
+				{
+					if (maxPrimes < numberPrimes)
+					{
 						maxPrimes = numberPrimes;
 						maxi = i;
 						maxj = j;
@@ -35,14 +43,19 @@ int main(int argc, char ** argv){
 	return 0;
 }
 
-int Quadratic(int a, int b, int n){
+int Quadratic(int a, int b, int n)
+{
 	return (n * n) + (a * n) + (b);
 }
 
-bool isPrime(int number){
-	if(number <= 1) return false;
-	for(int i = 2; i < number; i++){
-		if(number % i == 0) return false;
+bool isPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	for (int i = 2; i < number; i++)
+	{
+		if (number % i == 0)
+			return false;
 	}
 	return true;
 }
