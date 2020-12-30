65c91a0130c7088d94650a5d96d07eb407c3d340
Alireza <Alirezakamyab19@gmail.com>
2020-12-30 19:00:18 +0330

    Problem 31 Optimized

--- CoinSums.cpp
+++ CoinSums.cpp
@@ -1,48 +1,38 @@
 // Created on: Dec 27, 2020 by Alireza
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
 using namespace std;
 
-int CoinSums(int number);
+int ways(int target, vector<int>::iterator coin);
 
 int main(int argc, char **argv) {
+	vector<int> units = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	vector<int>::iterator it = find(units.begin(), units.end(), 200);
 
-	cout << CoinSums(200) << endl;
+	cout << ways(200,it) << endl;
 	return 0;
 }
 
-int CoinSums(int number) {
-	int ways = 0;
-	int a = 1, b = 2, c = 5, d = 10, e = 20, f = 50, g = 100, h = 200;
-	for (int hCounter = 0; hCounter <= number / h; hCounter++) {
-		for (int j = 0; j <= number / g; j++) {
-			for (int fCounter = 0; fCounter <= number / f; fCounter++) {
-				for (int eCounter = 0; eCounter <= number / e; eCounter++) {
-					for (int dCounter = 0; dCounter <= number / d; dCounter++) {
-						for (int cCounter = 0; cCounter <= number / c;
-								cCounter++) {
-							for (int bCounter = 0; bCounter <= number / b;
-									bCounter++) {
-								for (int aCounter = 0; aCounter <= number / a;
-										aCounter++) {
-									if (hCounter * h + j * g + fCounter * f
-											+ eCounter * e + dCounter * d
-											+ cCounter * c + bCounter * b
-											+ aCounter * a > number)
-										break;
-									if (hCounter * h + j * g + fCounter * f
-											+ eCounter * e + dCounter * d
-											+ cCounter * c + bCounter * b
-											+ aCounter * a == number)
-										ways++;
-									else
-										continue;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+// Description : Recursive Solution
+// If we have target as 5 and the greatest coin to be broken into as 5
+// We have one 5coin and none of the smaller ones
+// Or we have zero 5coins and the smaller ones
+// The same goes for smaller ones
+// Base : If coin is equal to 1 : return 1
+map<pair<int,int>,int> memo;
+int ways(int target, vector<int>::iterator coin) {
+	if (*coin <= 1 || target == 0)
+		return 1;
+	int result = 0;
+	auto it = memo.find(pair<int,int>{target,*coin});
+	if(it != memo.end()) return it-> second;
+	while (target >= 0) {
+		result += ways(target, coin - 1);
+		target = target - *coin;
 	}
-	return ways;
+	memo.insert({{target, *coin}, result});
+	return result;
 }
