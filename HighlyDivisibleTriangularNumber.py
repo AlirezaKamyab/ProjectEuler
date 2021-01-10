dd1db9b9e20b8a1d557aac4fb6e36dd77f34edc4
Alireza <Alirezakamyab19@gmail.com>
2021-01-10 13:40:40 +0330

    Problem 12 Optimized

--- HighlyDivisibleTriangularNumber.py
+++ HighlyDivisibleTriangularNumber.py
@@ -1,27 +1,72 @@
 #!/usr/bin/python3
 import math
+
+primeList = [2]
+
+
 def main():
-    for number in TriangularGenerator():
-        count = countDivisions(number)
-        print(number,"---",count)
-        if count > 500:
-            print("The answer is", number)
+    for i in primeGenerator():
+        if(i > 10000):
             break
+        primeList.append(i)
 
-def TriangularGenerator():
-    counter, result = 1, 0
-    while True:
-        result += counter
-        counter += 1
-        yield result
-        
-def countDivisions(number):
-    counter, numberofDivisions = 1, 0
-    while counter <= int(math.sqrt(number)):
-        if number % counter == 0:
-            numberofDivisions += 2
-        counter += 1
+    i = 0
+    counter = 1
+    t = 1
+    while counter <= 500:
+        counter = 1
+        i += 1
+        t = TriangularNumber(i)
+        tt = t
+        for p in primeList:
+            if p**2 > tt:
+                counter *= 2
+                break
+            
+            exponent = 1
+            while tt % p == 0:
+                exponent += 1
+                tt = tt / p
+            else:
+                if exponent > 1: counter *= exponent
+                if tt == 1: break
     else:
-        return numberofDivisions
+        print(t)
 
-if __name__ == '__main__': main()
+def TriangularNumber(n):
+    return (n * (n + 1)) // 2
+
+
+def isPrime(n):
+    if n == 1:
+        return False
+    if n < 4:
+        return True
+    if n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    f = 5
+    r = math.floor(math.sqrt(n))
+    while f <= r:
+        if(n % f == 0):
+            return False
+        if(n % (f + 2) == 0):
+            return False
+        f += 6
+    else:
+        return True
+
+
+def primeGenerator():
+    counter = 3
+    while True:
+        if(isPrime(counter)):
+            yield counter
+        counter += 2
+
+
+if __name__ == '__main__':
+    main()
