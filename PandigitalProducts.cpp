e1b1040c2bd11858141d1e1c2086fde849199077
Alireza <Alirezakamyab19@gmail.com>
2021-03-28 18:34:54 +0430

    Optimization of solution 32

--- PandigitalProducts.cpp
+++ PandigitalProducts.cpp
@@ -1,70 +1,70 @@
 //Created on: Jan 1, 2021 by Alireza
-#include<iostream>
-#include<set>
+#include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-bool isPandigital(int a , int b);
-int length(int a);
+int digits(int number);
+bool isPandigital(int a, int b);
+int isExisted(vector<int> vec, int number);
 
-template<typename t>
-t sum(set<t> nums);
-
-int main(int argc, char ** argv){
-	set<int> answers;
-	for(int i = 1; true; i++){
-		if(i % 10 == 0) continue; // 0 is not considered as a member of Pandigitals
-		if(length(i) > 4) break; // If I gets to be 5 digits, minimum digit of identity and product will be 11
-		for(int j = 1; true; j++){
-			if(length(i) + length(j) + length(i*j) >= 10) break;
-			if(isPandigital(i, j)){
-				answers.insert(i*j);
+int main() {
+	int summation = 0;
+	vector<int> products;
+	for (int i = 1;; i++) {
+		if (digits(i) > 3) break; // If I is 4 digit so j = i + 1 and the product will have more than 9
+		for (int j = i + 1;; j++) {
+			if (digits(j) + digits(i) > 5) break; // If digits of i and j exceeds 5, the product will be more than 9 digits
+			int product = i * j;
+			if (isPandigital(i, j) && !isExisted(products, product)) {
+				summation += product;
+				products.push_back(product);
 			}
 		}
 	}
 
-	cout << "The answer is " << sum<int>(answers) << endl;
+	cout << summation << endl;
 	return 0;
 }
 
-bool isPandigital(int a , int b){
-	string strNumbers = to_string(a * b) + to_string(a) + to_string(b);
-	size_t length = strNumbers.length();
-	set<int> numbers;
-	if(a % 10 == 0 || b % 10 == 0) return false;
-	for(char i : strNumbers){
-		int n = i - '0';
-		if (n == 0) return false;
-		pair<set<int>::iterator, bool> result;
-		result = numbers.insert(n);
-		if(!result.second) return false;
-	}
-	if(numbers.size() != length){
-		return false;
-	}
+bool isPandigital(int a, int b) {
+	int* numbers = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int product = a * b;
 
-	set<int> target = {1,2,3,4,5,6,7,8,9};
-	if(target != numbers) return false;
+	int a_digits = digits(a);
+	int b_digits = digits(b);
+	int c_digits = digits(product);
+	if (a_digits + b_digits + c_digits != 9) return false;
 
-	return true;
-}
-
-// Returns an integer representing how many digits does a number have
-int length(int a){
-	int counter = 0;
-	while(a >= 1){
-		counter++;
+	while (a != 0) {
+		numbers[(a % 10) - 1]++;
 		a /= 10;
 	}
 
-	return counter;
+	while (b != 0) {
+		numbers[(b % 10) - 1]++;
+		b /= 10;
+	}
+
+	while (product != 0) {
+		numbers[(product % 10) - 1]++;
+		product /= 10;
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (numbers[i] == 0) return false;
+		if (numbers[i] > 1) return false;
+	}
+	return true;
 }
 
-// returns an integer of sum of all the numbers in a set
-template<typename t>
-t sum(set<t> nums){
-	int summation = 0;
-	for(t i : nums){
-		summation += i;
+int digits(int number) {
+	return int(log10(number)) + 1;
+}
+
+int isExisted(vector<int> vec, int number) {
+	for (vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+		if (*it == number) return true;
 	}
-	return summation;
+	return false;
 }
