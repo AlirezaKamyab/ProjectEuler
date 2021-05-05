9996045513e213d81d3cc98b1f0c0444f7030242
Alireza Kamyab <alirezakamyab19@gmail.com>
2021-05-05 15:42:59 +0430

    optimization for problem 4

--- LargestPalindromeProduct.py
+++ LargestPalindromeProduct.py
@@ -1,25 +1,35 @@
 #!/usr/bin/Python3
 
 def main():
-    i,j = 100,100
-    answer = 0
-    while i < 1000:
-        while j < 1000:
-            if i*j == getReverse(i*j) and i*j > answer:
-                answer = i * j
-            j += 1
+    i, j = 999, 999
+    largestPalindrome = 0
+    while i >= 100:
+        if i % 11 == 0:
+            j = 999
+            db = 1
         else:
-            j = 100
-            i += 1
-    else:
-        print("The answer is {}".format(answer))
+            j = 990 #divsible by 11
+            db = 11
+            
+        while j >= i:
+            if i * j < largestPalindrome:
+                break
 
-def getReverse(number): #Returns a reversed number of the number given as parameter
+            if i * j == getReverse(i * j):
+                largestPalindrome = i * j
+
+            j -= db
+        i -= 1
+    print(largestPalindrome)
+
+
+def getReverse(number):  # Returns a reversed number of the number given as parameter
     result = 0
     while number >= 1:
         result = (result * 10) + (number % 10)
         number = number // 10
     else:
         return result
+
 
 if __name__ == "__main__": main()
