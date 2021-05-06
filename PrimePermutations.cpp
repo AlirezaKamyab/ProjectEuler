fb0b6af09087baf0c2db9ec88b3f70c1d5da9596
Alireza Kamyab <alirezakamyab19@gmail.com>
2021-05-06 11:23:37 +0430

    Solution to problem 49

---
+++ PrimePermutations.cpp
@@ -0,0 +1,80 @@
+// This file is written by Alireza Kamyab 5/6/2021
+// Answer to problem projecteuler.net/problem=49
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int number);
+bool isPermutation(int a, int b);
+int search(int* arr, const int n, int value);
+
+int main(){
+    for(int i = 1488; i < 10000; i++){
+        if(isPrime(i) == false) continue;
+        for(int j = i + 1; j < 10000; j++){
+            if(isPrime(j) == false) continue;
+            if(isPermutation(i,j) == false) continue;
+            int diff = j - i;
+            int k = j + diff;
+            if(isPrime(k) && isPermutation(k, j)){
+                printf("%d%d%d\n", i, j, k);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
+
+bool isPermutation(int a, int b){
+    int aIndex = (int)log10(a) + 1; 
+    int bIndex = (int)log10(b) + 1; 
+    int* arrA = new int[aIndex];
+    int* arrB = new int[bIndex];
+
+    for(int i = 0; i < aIndex; i++){
+        arrA[i] = a % 10;
+        a /= 10;
+    }
+
+    for(int i = 0; i < bIndex; i++){
+        arrB[i] = b % 10;
+        b /= 10;
+    }
+
+    if(aIndex != bIndex) return false;
+
+    for(int i = 0; i < aIndex; i++){
+        int index = search(arrB, bIndex, arrA[i]);
+        if(index == -1) return false;
+        else arrB[index] = -1;
+    }
+
+    return true;
+}
+
+int search(int* arr, int n, int value){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == value) return i;
+    }
+
+    return -1;
+}
+
+bool isPrime(int number){
+    if(number <= 1) return false;
+    if(number < 4) return true;
+    if(number % 2 == 0) return false;
+    if(number < 9) return true;
+    if(number % 3 == 0) return false;
+
+    int counter = 1;
+    int limit = 0;
+    while(limit * limit <= number){
+        limit = counter * 6;
+        if(number % (limit + 1) == 0) return false;
+        if(number % (limit - 1) == 0) return false;
+        counter++;
+    }
+
+    return true;
+}
