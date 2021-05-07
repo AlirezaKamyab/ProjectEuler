222427439c2d62cf90033b5fe5bb46618aab1926
Alireza Kamyab <alirezakamyab19@gmail.com>
2021-05-07 17:09:50 +0430

    Optimization for problem 50

--- ConsecutivePrimeSum.cpp
+++ ConsecutivePrimeSum.cpp
@@ -1,71 +1,79 @@
 // This file is written by AlirezaKamyab 5/7/2021
 // Answer to problem projecteuler.net/problem=50
+// Using https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Overview
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
-bool isPrime(int number);
-int nextPrime(int number);
-void generatePrimeConsecutive(int initiate, int limit, int& maxStage, int& longestConsecutive);
-void findLongestConsecutivePrime(int limit, int& count, int& longestConsecutive);
+bool* primeArray(int limit);
+bool isPrime(bool* arr, uint64_t number);
+int nextPrime(bool* arr, int number, int limit);
+void generatePrimeConsecutive(bool* arr, int initiate, int limit, int& maxStage, uint64_t& longestConsecutive);
+void findLongestConsecutivePrime(bool* arr, int limit, int& count, uint64_t& longestConsecutive);
 
 int main(){
+    const int n = 1000000;
+    bool* primes = primeArray(n);
     int count = 0;
-    int longestConsecutive = 0;
-    findLongestConsecutivePrime(1000000, count, longestConsecutive);
+    uint64_t longestConsecutive = 0;
+    findLongestConsecutivePrime(primes, n, count, longestConsecutive);
     cout << "Max stage is " << count << " Longest consecutive is " << longestConsecutive << endl;
     return 0;
 }
 
-void findLongestConsecutivePrime(int limit, int& count, int& longestConsecutive){
+void findLongestConsecutivePrime(bool* arr, int limit, int& count, uint64_t& longestConsecutive){
     int counter = 2;
     while (counter < limit){
-        generatePrimeConsecutive(counter, limit, count, longestConsecutive);
-        counter = nextPrime(counter);
+        generatePrimeConsecutive(arr, counter, limit, count, longestConsecutive);
+        counter = nextPrime(arr, counter, limit);
     }
 }
 
-void generatePrimeConsecutive(int initiate, int limit, int& maxStage, int& longestConsecutive){
+void generatePrimeConsecutive(bool* arr, int initiate, int limit, int& maxStage, uint64_t& longestConsecutive){
     int stage = 1;
     int counter = initiate;
-    int sum = counter;
+    uint64_t sum = counter;
 
     while (sum <= limit){
-        counter = nextPrime(counter);
+        counter = nextPrime(arr, counter, limit);
         sum += counter;
         stage++;
 
-        if (sum > limit) return;
-        if (stage > maxStage && isPrime(sum)){
+        if (sum > limit) {
+            return;
+        }
+        if (stage > maxStage && isPrime(arr, sum)){
             maxStage = stage;
             longestConsecutive = sum;
         }
     }
 }
 
-int nextPrime(int number){
+int nextPrime(bool* arr, int number, int limit){
     for(int i = number + 1; true; i++){
-        if (isPrime(i)) return i;
+        if(i > limit) return limit + 1;
+        if (isPrime(arr, i)) return i;
     }
 
     return 0; // Never is going to be called
 }
 
-bool isPrime(int number){
-    if (number <= 1) return false;
-    if (number < 4) return true;
-    if (number % 2 == 0) return false;
-    if (number < 9) return true;
-    if (number % 3 == 0) return false;
+bool isPrime(bool* arr, uint64_t number){
+    return arr[number];
+}
 
-    int counter = 1;
-    int limit = 0;
+bool* primeArray(int limit){
+    bool* arr = new bool[limit + 1];
+    for(int i = 0; i < limit + 1; i++) arr[i] = true;
+    arr[0] = false;
+    arr[1] = false;
 
-    while (limit * limit <= number){
-        limit = counter * 6;
-        if (number % (limit + 1) == 0) return false;
-        if (number % (limit - 1) == 0) return false;
-        counter++;
+    for(int i = 2; i * i <= limit; i++){
+        if (arr[i] == false) continue;
+        for(int j = i * i; j <= limit; j += i){
+            arr[j] = false;
+        }
     }
 
-    return true;
+    return arr;
 }
