4ab2c054078a3d25a7af2769ebdf803fb6734452
Alireza Kamyab <alirezakamyab19@gmail.com>
2021-05-08 10:15:22 +0430

    optimization for problem 3

--- LargestPrimeFactor.py
+++ LargestPrimeFactor.py
@@ -3,19 +3,28 @@
 def main():
     number = 600851475143
     print(GreatestPrimeFactor(number))
-
-def SmallestPrimeFactor(number):
-    for i in range(2, int(number)):
-        if number % i == 0:
-            return i
-    else:
-        return number
     
 def GreatestPrimeFactor(number):
-    while True:
-        smallest = SmallestPrimeFactor(number)
-        if number > smallest:
-            number = number / smallest
-        else: return number
+    lastFactor = 0
+    if number % 2 == 0:
+        number = number / 2
+        lastFactor = 2
+        while number % 2 == 0:
+            number /= 2
+    else:
+        lastFactor = 1
+
+    factor = 3
+    while number > 1 and factor * factor <= number:
+        if number % factor == 0:
+            number /= factor
+            lastFactor = factor
+            while number % factor == 0:
+                number /= factor
+        factor += 2
+
+    if number == 1:
+        return lastFactor
+    else: return number
 
 if __name__ == "__main__":main()
