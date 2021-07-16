081991195971c871b8d41dc431f65ffd5afdfb04
AlirezaKamyab <alirezakamyab19@gmail.com>
2021-07-17 00:48:49 +0430

    Optimized solution for problem18

--- MaximumPathSumI.cpp
+++ MaximumPathSumI.cpp
@@ -3,61 +3,42 @@
 #include <fstream>
 using namespace std;
 
-int findPath(int**, const int, int, int = 0, int = 0, int = 0);
-int findAll(int** A, const int l);
+void findPath(int** A, const int l);
 
-void makeArray(int** numbers, const int n, string fileName = "MaximumPathSumI.txt");
+// Converting file to array
+void makeArray(int** numbers, const string fileName = "MaximumPathSumI.txt");
 void addRow(int** numbers, const int n, const string value);
 int convertToNumber(const string value);
+int countRows(const string fileName = "MaximumPathSumI.txt");
 
-const int MAX_ROWS = 15;
+int MAX_ROWS = countRows();
 
 int main(){
     int** A = new int*[MAX_ROWS];
-    for(int i = 0; i < MAX_ROWS; i++){
-        A[i] = new int[i + 1];
-    }
+    for(int i = 0; i < MAX_ROWS; i++) A[i] = new int[i + 1];
 
-    makeArray(A, MAX_ROWS);
-    cout << findAll(A, MAX_ROWS - 1) << endl;
-    
+    makeArray(A);
+    findPath(A, MAX_ROWS - 1);
+    cout << A[0][0] << endl;
 
     for(int i = 0; i < 4; i++) delete[] A[i];
     delete[] A;
     return 0;
 }
 
-int findPath(int** A, const int l, int n, int previousN, int sum, int maxSum){
-    sum += A[l][n];
-
-    // Base cases
-    if(n < previousN - 1 || n > previousN) return maxSum;
-    if(n > l || n < 0) return maxSum;
+void findPath(int** A, const int l){
     if(l == 0){
-        if(maxSum < sum) maxSum = sum;
-        return maxSum;
+        return;
     }
 
-    // Recursive
-    if(maxSum < sum) maxSum = sum;
-    int a = findPath(A, l - 1, n, n, sum, maxSum);
-    int b = findPath(A, l - 1, n - 1, n, sum, maxSum);
-    if(a > b) return a;
-    if(b > a) return b;
-    return a;
-}
-
-int findAll(int** A, const int l){
-    int max = 0;
     for(int i = 0; i < l; i++){
-        int temp = findPath(A, l, i, i);
-        if(max < temp) max = temp;
+        if(A[l][i] >= A[l][i + 1]) A[l - 1][i] += A[l][i];
+        else A[l - 1][i] += A[l][i + 1];
     }
-
-    return max;
+    findPath(A, l - 1);
 }
 
-void makeArray(int** numbers, const int n, string fileName){
+void makeArray(int** numbers, const string fileName){
     fstream stream;
     stream.open(fileName, ios::in);
     int row = 0;
@@ -66,8 +47,22 @@ void makeArray(int** numbers, const int n, string fileName){
         while(getline(stream, strnum)){
             addRow(numbers, row++, strnum);
         }
+        stream.close();
     }
-    stream.close();
+}
+
+int countRows(const string fileName){
+    fstream stream;
+    stream.open(fileName, ios::in);
+    int row = 0;
+    if(stream.is_open()){
+        string strnum;
+        while(getline(stream, strnum)){
+            row++;
+        }
+        stream.close();
+    }
+    return row;
 }
 
 void addRow(int** numbers, const int n, const string value){
