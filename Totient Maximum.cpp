cad3bdb4863e661a1b99ac66c3c343547e0ac133
Alireza Kamyab <alirezakamyab19@gmail.com>
2024-02-05 22:07:52 +0330

    Problem 69

---
+++ Totient Maximum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void generate_phi(int* arr, const int& limit);
+
+constexpr int LIMIT = 1e6 + 1;
+int arr[LIMIT];
+
+int main() {
+    generate_phi(arr, LIMIT);
+    float max = 0;
+    int max_n = 0;
+    for(int i = 2; i < LIMIT; i++) {
+        if(i / (float)arr[i] > max) {
+            max = i / (float)arr[i];
+            max_n = i;
+        }
+    }
+
+    cout << max_n << endl;
+    return 0;
+}
+
+void generate_phi(int* arr, const int& limit) {
+    for(int i = 0; i < limit; i++) arr[i] = i;
+
+    for(int i = 2; i * i <= limit; i++) {
+        if(arr[i] == i) arr[i] = i - 1;
+        else continue;
+        for(int j = 2 * i; j <= limit; j += i) {
+            arr[j] = arr[j] - arr[j] / i;
+        }
+    }
+}
